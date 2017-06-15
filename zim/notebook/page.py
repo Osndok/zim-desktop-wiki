b1397a1d22f124442f7065d882a65a752bb6d412
Robert Hailey <git@osndok.com>
2017-06-15 16:42:24 -0500

    Revert "bug: cursor, selection bounds, and scroll values are lost when appending text via a plugin"
    This reverts commit 529afb1f4e1a1442bcbf75a34dc338c61255ea21.

--- zim/notebook/page.py
+++ zim/notebook/page.py
@@ -526,7 +526,6 @@ class Page(Path, SignalEmitter):
 
 		if self._ui_object:
 			self._ui_object.set_parsetree(tree)
-			self._ui_object.scroll_cursor_on_screen()
 		else:
 			self._parsetree = tree
 
