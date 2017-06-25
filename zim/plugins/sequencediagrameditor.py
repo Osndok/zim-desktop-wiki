0dacb07db128cf329f593545fea07c3cf513acb2
Robert Hailey <git@osndok.com>
2017-06-25 12:48:02 -0500

    F: seq diagram plugin (SVG, error messages)

--- zim/plugins/sequencediagrameditor.py
+++ zim/plugins/sequencediagrameditor.py
@@ -1,71 +1,99 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2011 Greg Warner <gdwarner@gmail.com>
-# (Pretty much copied from diagrameditor.py)
+# Copyright 2009 Jaap Karssenberg <jaap.karssenberg@gmail.com>
 
+import subprocess
+import gtk
 
-from zim.plugins import PluginClass
 from zim.plugins.base.imagegenerator import ImageGeneratorPlugin, ImageGeneratorClass
-
 from zim.fs import File, TmpFile
+from zim.config import data_file
 from zim.applications import Application, ApplicationError
 
 
 # TODO put these commands in preferences
-diagcmd = ('seqdiag', '-o')
-
+dotcmd = ('mscgen', '-Tsvg', '-o')
 
 class InsertSequenceDiagramPlugin(ImageGeneratorPlugin):
 
 	plugin_info = {
 		'name': _('Insert Sequence Diagram'), # T: plugin name
 		'description': _('''\
-This plugin provides a sequence diagram editor for zim based on seqdiag.
-It allows easy editing of sequence diagrams.
+This plugin provides a diagram editor for zim based on GraphViz.
+
+This is based on the diagrameditor core plugin that ships with zim, but
+obscures the default sequencediagrameditor that would ordinarily be used.
 '''), # T: plugin description
-		'help': 'Plugins:Sequence Diagram Editor',
-		'author': 'Greg Warner',
+		'help': 'Plugins:Diagram Editor',
+		'author': 'Jaap Karssenberg',
 	}
 
-	object_type = 'seqdiagram'
-	short_label = _('Sequence Diagram') # T: menu item
+	object_type = 'mscdiagram'
+	short_label = _('Sequence Diagram...') # T: menu item
 	insert_label = _('Insert Sequence Diagram') # T: menu item
 	edit_label = _('_Edit Sequence Diagram') # T: menu item
-	syntax = None
+	syntax = 'dot'
 
 	@classmethod
 	def check_dependencies(klass):
-		has_diagcmd = Application(diagcmd).tryexec()
-		return has_diagcmd, [("seqdiag", has_diagcmd, True)]
+		has_dotcmd = Application(dotcmd).tryexec()
+		return has_dotcmd, [("GraphViz", has_dotcmd, True)]
 
 
 class SequenceDiagramGenerator(ImageGeneratorClass):
 
 	uses_log_file = False
 
-	object_type = 'seqdiagram'
-	scriptname = 'seqdiagram.diag'
-	imagename = 'seqdiagram.png'
+	object_type = 'mscdiagram'
+	scriptname = 'diagram.msc'
+	imagename = 'diagram.svg'
 
 	def __init__(self, plugin):
 		ImageGeneratorClass.__init__(self, plugin)
-		self.diagfile = TmpFile(self.scriptname)
-		self.diagfile.touch()
-		self.pngfile = File(self.diagfile.path[:-5] + '.png') # len('.diag') == 5
+		self.dotfile = TmpFile(self.scriptname)
+		self.dotfile.touch()
+		#self.logfile = TmpFile(self.scriptname+'.log')
+		self.svgfile = File(self.dotfile.path[:-4] + '.svg') # len('.dot') == 4
 
 	def generate_image(self, text):
 		# Write to tmp file
-		self.diagfile.write(text)
+		self.dotfile.write(text)
 
-		# Call seqdiag
-		try:
-			diag = Application(diagcmd)
-			diag.run((self.pngfile, self.diagfile))
-		except ApplicationError:
-			return None, None # Sorry, no log
+		# Call GraphViz
+
+		argv = dotcmd + tuple(map(unicode, (self.svgfile.path, self.dotfile.path)));
+		p = subprocess.Popen(argv, cwd=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = p.communicate(text)
+		
+		output = self._read_all(stdout)
+		diagnostics = self._read_all(stderr)
+		
+		#self.logfile.write(stderr)
+		
+		if p.returncode == 0:
+			if self.svgfile.exists():
+				return self.svgfile, None
+			else:
+				return None, None;
 		else:
-			return self.pngfile, None
+			# Present a [modal?] dialog box with the diagnostic output & exit code.
+			m = gtk.MessageDialog(
+								  gtk.Window(),
+								  gtk.DIALOG_MODAL,
+								  gtk.MESSAGE_ERROR,
+								  gtk.BUTTONS_NONE,
+								  "Could not create image"
+								  )
+			m.format_secondary_text("\n".join(diagnostics + ["Code: %s" % p.returncode]))
+			m.show()
+			return None, None;
+
+	def _read_all(self, stdout):
+		text = [unicode(line + '\n', errors='replace') for line in stdout.splitlines()]
+		if text and text[-1].endswith('\n') and not stdout.endswith('\n'):
+			text[-1] = text[-1][:-1] # strip additional \n
+		return text
 
 	def cleanup(self):
-		self.diagfile.remove()
-		self.pngfile.remove()
+		self.dotfile.remove()
+		self.svgfile.remove()
